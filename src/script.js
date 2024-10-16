ecac631724d6424ff271577b12c3ee09178e72bc
AngelB77 <angelb1977@gmail.com>
2024-10-16 11:45:48 +0200

    merge remote feature/drumset to local

--- src/script.js
+++ src/script.js
@@ -144,7 +144,9 @@ function playSnareDrum () {
     snareAudio.currentTime = 0;
     snareAudio.play()
 };
+
 snareDrum.addEventListener("click",playSnareDrum);
+
 document.addEventListener("keydown", function(event) {
     if(event.key === "s" || event.key === "s"){
         playSnareDrum();
